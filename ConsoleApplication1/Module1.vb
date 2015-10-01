48c421aebcfcef2b3298e00c42f34dc62152f478
小朱 <regionbbs@gmail.com>
2015-10-01 20:34:16 +0800

    作業

---
+++ ConsoleApplication1/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+
+        For i As Integer = 1 To 9
+            For j As Integer = 1 To 9
+                Console.WriteLine("{0}x{1}={2}", i, j, i * j)
+            Next
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
